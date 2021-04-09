7b1cb9781e1b82b108d39c741bc8757c12aba72a
Sitare Arslanturk <sitare.arslanturk@ykteknoloji.com.tr>
2021-04-10 01:56:56 +0300

    First commit

---
+++ app/src/main/java/com/sitare/localization/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.sitare.localization
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
+import com.sitare.localization.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+       // binding.loginButton.text = "Continue"
+        //binding.emailTextInput.setText("sarslanturk16@ku.edu.tr")
+
+        binding.loginButton.setOnClickListener {
+         validate()
+        }
+    }
+
+    private fun validate(){
+        if(!validateEmail() && validatePassword().not()) return
+        if(!validateEmail() || validatePassword().not()) return
+        val email:String? = binding.emailTextInput.text?.toString()
+        val username = email?.substringBefore("@")
+        showToast(getString(R.string.welcome, username))
+    }
+
+    private fun validateEmail():Boolean{
+        var email:String? = binding.emailTextInput.text?.toString()
+        // email?.let { showToast(it) }
+        if(email.isNullOrEmpty()){
+            binding.emailTextInputLayout.error = getString(R.string.empty_email_warning)
+            return false
+        }
+
+        if (email.contains("@").not()){
+            binding.emailTextInputLayout.error = getString(R.string.invalid_email_warning)
+            return false
+        }
+        binding.emailTextInputLayout.error = null
+        return true
+    }
+
+    private fun validatePassword(): Boolean{
+        var password:String? = binding.passwordTextInput.text?.toString()
+        val passwordLength = password?.length ?: 0
+        if(passwordLength < 8){
+            binding.passwordTextInputLayout.error = getString(R.string.empty_password_warning)
+            return false
+        }
+        binding.passwordTextInputLayout.error = null
+        return true
+    }
+
+    private fun showToast(text: String){
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+    }
+}
